3f0f08de8aa24b5a788505a6b8ece2c8b37794e3
Chris Wiechmann <cwiechmann@axway.com>
2021-07-12 17:18:20 +0200

    Adding dummy origin header

--- src/main/java/com/axway/oauth/client/providers/AzureOAuth2Provider.java
+++ src/main/java/com/axway/oauth/client/providers/AzureOAuth2Provider.java
@@ -1,5 +1,14 @@
 package com.axway.oauth.client.providers;
 
-public class AzureOAuth2Provider extends BaseOAuth {
+import com.vordel.oauth.client.OAuthTokenRequest;
+import com.vordel.oauth.client.providers.BaseOAuth2Provider;
 
+public class AzureOAuth2Provider extends BaseOAuth2Provider {
+
+	@Override
+	public OAuthTokenRequest modifyRequest(OAuthTokenRequest tokenRequest) {
+		// Azure is only validating the presence of the Origin header, but not the actual value
+		tokenRequest.addHeader("Origin", "Dummy");
+		return tokenRequest;
+	}
 }
